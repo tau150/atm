91fa58e68ac5d96decc72af4b9719a4bc73ec817
Santiago Nuñez <tau150@hotmail.com>
2022-08-24 21:41:01 +0200

    change data file location

--- helpers/users-repo.ts
+++ helpers/users-repo.ts
@@ -2,13 +2,13 @@ import { User } from "types";
 
 const fs = require("fs");
 
-const data = require("data/db.json");
+const data = require("public/data/db.json");
 
 function saveData(user: User): void {
   const restOfUsers = data.filter((dbUser: User) => dbUser.document !== user.document);
   const newUsers = [...restOfUsers, user];
 
-  fs.writeFileSync("data/db.json", JSON.stringify(newUsers, null, 4));
+  fs.writeFileSync("public/data/db.json", JSON.stringify(newUsers, null, 4));
 }
 
 export function getByDocument(document: number): User {
